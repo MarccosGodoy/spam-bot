415a4b5229506368618121a48ab3d6358a86b782
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-02-13 23:21:55 -0600

    🎁 With open
    Incluí todo dentro de un with por buena práctica (y hacer que el archivo se cierre siempre, aún ocurra una excepción).

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import pyautogui
 import time
 
-f = open("spam.txt", "r")
-time.sleep(5)
-for words in f:
-    print(words)
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
-    time.sleep(3)
+with open("spam.txt", "r") as f:
+    time.sleep(5)
+    for words in f:
+        print(words)
+        pyautogui.typewrite(words)
+        pyautogui.press("enter")
+        time.sleep(3)
